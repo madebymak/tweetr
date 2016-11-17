620f4ab999a108cdb0c03f9227441e26ca8f4557
madebymak <mr.jmak@gmail.com>
2016-11-17 20:43:45 +0000

    Added loadTweets function to get JSON data

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,56 +7,77 @@
 // Test / driver code (temporary). Eventually will get this from the server.
 $(function() {
 
-  var tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
+//   var tweetData = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": {
+//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+//       },
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": {
+//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+//       },
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   },
+//   {
+//     "user": {
+//       "name": "Johann von Goethe",
+//       "avatars": {
+//         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+//         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+//         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+//       },
+//       "handle": "@johann49"
+//     },
+//     "content": {
+//       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+//     },
+//     "created_at": 1461113796368
+//   }
+// ];
+
+
+
+  //version 1
+  function loadTweets() {
+      var getTweets = $.ajax({
+      dataType: "JSON",
+      url: "http://localhost:8080/tweets/",
+      method: "GET",
+      success: function (data) {
+         console.log(renderTweets(data))
+         // console.log(JSON.stringify(data));
+      }
+    })
+     // return tweetObject;
+     // console.log(JSON.stringify(tweetObject));
   }
-];
+
+loadTweets();
+
+
 
   ///////////////
   function renderTweets(tweets) {
-    console.log(tweets);
+    // console.log(tweets);
 
     tweets.forEach(function (data) {
       createTweetElement(data);
@@ -98,8 +119,10 @@ $(function() {
               <div class="icons">
                 <i class="fa fa-flag"></i>
                 <i class="fa fa-retweet"></i>
-                <i class="fa fa-heart"></i></div>
+                <i class="fa fa-heart"></i>
               </div>
+            </div>
+            <div class="blank"></div>
           </footer>
         </article>`
     );
@@ -145,7 +168,7 @@ $(function() {
 ///////////////
 
 // createTweetElement(tweetData)
- renderTweets(tweetData);
+ // renderTweets(loadTweets());
 
 
 
