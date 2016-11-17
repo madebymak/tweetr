0cbb77e5606417455610c399d46af49fb9a43375
madebymak <mr.jmak@gmail.com>
2016-11-17 21:47:35 +0000

    Added character check messgages (no characters enter or too many characters)

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,26 @@
 $( document ).ready(function() {
  console.log("load ok!");
 
- $(".textBox").keyup(function (x) {
-  var charMax =140;
+ $(".text-box").keyup(function (x) {
+  var charMax = 140;
   var length = $(this).val().length;
   var length = charMax - length;
+
+
   $(".counter").text(length)
-    if(length < 0){
-      $(".counter").text(length).css("color", "red");
+  // if no characters are entered
+    if (length === null){
+      $(".counter").text("Nothing entered.").text
     }
+
+    if (length <= 140){
+      $(".counter").text(length).css("color", "black").text;
+    }
+
+    if (length < 0){
+      $(".counter").text(length).css("color", "red").text("Too many.");
+    }
+
   });
 });
 
