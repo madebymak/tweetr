f4cdc25da0593811ce11e1aeefd5dd39f5e6cc28
madebymak <mr.jmak@gmail.com>
2016-11-18 00:07:06 +0000

    Added compose button on nav bar and css for it

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,53 +7,36 @@
 // Test / driver code (temporary). Eventually will get this from the server.
 $(function() {
 
-//   var tweetData = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//       },
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//       },
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   },
-//   {
-//     "user": {
-//       "name": "Johann von Goethe",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//       },
-//       "handle": "@johann49"
-//     },
-//     "content": {
-//       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-//     },
-//     "created_at": 1461113796368
-//   }
-// ];
+//serialize the submitted text
+  // $( "form" ).on( "submit", function( event ) {
+  //   event.preventDefault();
+  //   $.post("http://localhost:8080/tweets", {content: $( this ).serialize()});
 
+  //   // console.log( $( this ).serialize() );
+  // });
+
+
+  $('form[action="/tweets"]').on('submit', function (event) {
+    event.preventDefault();
+    var formInput = $(this);
+    // console.log(formInput.serialize());
+    $.ajax({
+      method: formInput.attr('method'),
+      url: formInput.attr('action'),
+      data: formInput.serialize(),
+      success: function (data){
+        console.log(data);
+        loadTweets(data);
+      }
+    })
+    // .done(function (newMessage) {
+    //   // The code inside the callback is the pessimistic code
+    //   renderMessage(newMessage);
+    // });
+    // This is where optimistic code goes - inside the function
+    // that is handling the user event, that is, the form submission
+    // theForm.find('input[name=messageText]').val('');
+  });
 
 
   //version 1
@@ -63,7 +46,7 @@ $(function() {
       url: "http://localhost:8080/tweets/",
       method: "GET",
       success: function (data) {
-         console.log(renderTweets(data))
+        renderTweets(data)
          // console.log(JSON.stringify(data));
       }
     })
@@ -71,7 +54,7 @@ $(function() {
      // console.log(JSON.stringify(tweetObject));
   }
 
-loadTweets();
+ loadTweets();
 
 
 
@@ -105,7 +88,7 @@ loadTweets();
       return div.innerHTML;
     }
 
-    $('.tweet-timeline').append(
+    $('.tweet-timeline').prepend(
     `<article>
           <header>
             <div class="header"><img class="avatar" src="${avatar}">
